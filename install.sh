e2ba514eff3e73d21023f83a47b4ef948cad9881
Adam Pilewski <apilewski01@gmail.com>
2022-10-28 00:10:34 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 cd backend
+pip3 install -U Flask 
+pip install python-dotenv
 pip3 install -U pytest
 pip3 install pytest-xdist
 pip3 install -r requirements.txt
-pip3 install flask
-cd ..
-cd frontend
+cd ../frontend
 npm install dotenv --save
 npm install react-router-dom@6
 npm install axios
-rm -r node_modules
+if [ -d node_modules ]; then rm -Rf node_modules; fi
 npm install
 cd ..
