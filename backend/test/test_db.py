682c4d195924d31d7c07ad35a1c649e56134676c
dc064 <dawidchmielewski064@gmail.com>
2022-10-28 01:21:19 +0200

    Add test with correct parameters for update

--- backend/test/test_db.py
+++ backend/test/test_db.py
@@ -38,7 +38,7 @@ def test_should_get_one_element_bigger_dict_after_correct_insertion():
     db = Database(CONST_DB_DILENAME)
     before = db.get_registrations()
     date=datetime.now()
-    new_reg ={'date_1':date,'date_2':date, 'who':'xd','what':'xdddd'}
+    new_reg ={'date_1':date,'date_2':date, 'who':'Marek Nowakowski','what':'Awaria 1'}
     db.insert_registration(new_reg)
     after = db.get_registrations()
     assert len(before)+1==len(after)
@@ -46,8 +46,21 @@ def test_should_get_one_element_bigger_dict_after_correct_insertion():
 def test_should_get_correct_record_for_correct_reading_parameter():
     db = Database(CONST_DB_DILENAME)
     currentRegistration = db.get_registration_by_id(1)
-    assert currentRegistration['who'] == 'xd'
-    assert currentRegistration['what'] == 'xdddd'
+    assert currentRegistration['who'] == 'Marek Nowakowski'
+    assert currentRegistration['what'] == 'Awaria 1'
+
+def test_should_update_record_for_correct_update_parameters():
+    db = Database(CONST_DB_DILENAME)
+    cur_reg = db.get_registration_by_id(1)
+    assert cur_reg['who'] == 'Marek Nowakowski'
+    assert cur_reg['what'] == 'Awaria 1'
+    new_reg = {'date_1':cur_reg['date_1'],'date_2':cur_reg['date_2'], 'who':'Jan Kowalski','what':'Awaria 2'}
+    
+    db.update_registration(new_reg, 1)
+
+    cur_reg = db.get_registration_by_id(1)
+    assert cur_reg['who'] == 'Jan Kowalski'
+    assert cur_reg['what'] == 'Awaria 2'
 
 def test_should_get_one_element_smaller_dict_after_correct_deletion():
     db = Database(CONST_DB_DILENAME)
